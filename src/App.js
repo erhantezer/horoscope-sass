78feac17cb59cb41b2231c41e8c0892ba7a171d5
Erhan <erhantezer68@gmail.com>
2023-02-19 02:46:35 +0300

    updated app js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
+import Navbar from "./components/Navbar/Navbar";
 
 
 function App() {
   return (
     <div>
-
+      <Navbar/>
+      <Header/>
+      <Main/>
     </div>
   );
 }

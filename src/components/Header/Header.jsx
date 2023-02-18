a9538076f3950109be54c74840d453dcaef2d277
Erhan <erhantezer68@gmail.com>
2023-02-19 02:58:32 +0300

    updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <span className="text-shadow">Who Are You ?</span>
+    </div>
+    );
 };
 
 export default Header;

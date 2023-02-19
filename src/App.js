c0c54391bfe06e3109bde18db20c7b87a01a5e55
Erhan <erhantezer68@gmail.com>
2023-02-19 20:29:31 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 import Navbar from "./components/Navbar/Navbar";
-
+import './App.scss';
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <Navbar/>
       <Header/>
       <Main/>

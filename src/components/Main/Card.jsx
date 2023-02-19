b968f3b9e266c3ed71f204d0c04cf441ae818eee
Erhan <erhantezer68@gmail.com>
2023-02-19 17:04:36 +0300

    updated

--- src/components/Main/Card.jsx
+++ src/components/Main/Card.jsx
@@ -1,11 +1,26 @@
 import React from "react";
-import useData from "../../hooks/useData";
 
-const Card = () => {
-  const { title, date, image, id, desc } = useData()
-  return <div className="cards">
+
+const Card = ({ id, title, date, image, desc }) => {
+
+  return (
+    <div className="cards" key={id}>
+
+      <div className="title">
+        <h1>{title}</h1>
+      </div>
+
+      <div className="date">
+        <h1>{date}</h1>
+      </div>
+
+      <img src={image} alt="" />
+
+      <div className="card-over">
+        <p>{desc}</p>
+      </div>
       
-  </div>;
+    </div>)
 };
 
 export default Card;
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,11 @@
 import Card from "./Card";
-
+import { data } from "../../helpers/data"
 
 const Main = () => {
   return (
     <div className="card-container">
-      <Card/>
+      {data.map((item, index) => <Card key={index} {...item} />)}
+
     </div>
   )
 
